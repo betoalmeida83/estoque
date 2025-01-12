cb15fbbeb105e33074783f6a5155cfe5aef6fcb2
betoalmeida83 <roberto.almeida5@fatec.sp.gov.br>
2025-01-12 12:09:35 -0300

    commit initial insert constructor/this

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -12,21 +12,22 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner input = new Scanner(System.in);
 
-        Product product = new Product();
         System.out.println("Enter product data: ");
         System.out.print("Name: ");
-        product.name = input.nextLine();
+        String name = input.nextLine();
         System.out.print("Price: ");
-        product.price = input.nextDouble();
+        double price = input.nextDouble();
         System.out.print("Quantity in stock: ");
-        product.quantity = input.nextInt();
+        int quantity = input.nextInt();
+
+        Product product = new Product(name, price, quantity);
 
         System.out.println();
         System.out.println("Product data: " + product);
 
         System.out.println();
         System.out.print("Enter the number of products to be added in stock: ");
-        int quantity = input.nextInt();
+        quantity = input.nextInt();
         product.addProducts(quantity);
 
         System.out.println();
@@ -39,7 +40,6 @@ public class Program {
 
         System.out.println();
         System.out.println("Update data: " + product);
-
 
         input.close();
 
